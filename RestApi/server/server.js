5984bb8e6eaef338684ea70e28450ee0f652cdde
vladovoev <vladovoev@yahoo.co.uk>
2018-05-06 16:57:08 +0300

    private routes and auth

--- RestApi/server/server.js
+++ RestApi/server/server.js
@@ -6,6 +6,7 @@ const { mongoose } = require('./mongoose/mongoose');
 const { ObjectID } = require('mongodb');
 const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
+const { authenticate } = require('./middleware/authenticate');
 
 const app = express();
 
@@ -98,6 +99,10 @@ app.post('/users', (req, res) => {
     }).catch((e) => {
         res.status(400).send(e);
     })
+})
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
 })
 
 app.listen(3000, () => {
