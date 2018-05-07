49d61f1910653842acc3ae441b4cfecfb6d26e84
vladovoev <vladovoev@yahoo.co.uk>
2018-05-07 12:43:03 +0300

    hash user password

--- RestApi/playground/hashing.js
+++ RestApi/playground/hashing.js
@@ -1,39 +1,53 @@
 const { SHA256 } = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
+const password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    })
+})
 
-var decoded = jwt.verify(token, '123abc');
-console.log(decoded);
-
-
-// var message = 'I am user';
-// var hash = SHA256(message).toString();
+var hashedPassword = '$2a$10$tH36AaY2uhbszgquahQznu.UfXze.MiYvxJwNT91pBlAcYyUB6Cp.';
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+})
 
 // var data = {
-//     id: 4
-// }
+//     id: 10
+// };
 
-// var token = {
-//     data,
-//     hash: SHA256(JSON.stringify(data) + 'someSecret').toString()
-// }
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
 
-// token.data.id = 5;
-// token.hash = SHA256(JSON.stringify(token.data)).toString();
+// var decoded = jwt.verify(token, '123abc');
+// console.log(decoded);
 
 
-// var resultHash = SHA256(JSON.stringify(token.data) + 'someSecret').toString();
+// // var message = 'I am user';
+// // var hash = SHA256(message).toString();
 
-// if (resultHash === token.hash) {
-//     console.log('Data was not changed');
-// } else {
-//     console.log('Data was changed');
-//     console.log(resultHash, token.hash);
+// // var data = {
+// //     id: 4
+// // }
 
-// }
+// // var token = {
+// //     data,
+// //     hash: SHA256(JSON.stringify(data) + 'someSecret').toString()
+// // }
+
+// // token.data.id = 5;
+// // token.hash = SHA256(JSON.stringify(token.data)).toString();
+
+
+// // var resultHash = SHA256(JSON.stringify(token.data) + 'someSecret').toString();
+
+// // if (resultHash === token.hash) {
+// //     console.log('Data was not changed');
+// // } else {
+// //     console.log('Data was changed');
+// //     console.log(resultHash, token.hash);
+
+// // }
