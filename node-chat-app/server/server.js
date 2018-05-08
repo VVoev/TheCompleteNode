9eb6ed0b674cac897a7c1e71f8a375e1b8dba0f6
vladovoev <vladovoev@yahoo.co.uk>
2018-05-08 10:06:31 +0300

    refactoring

--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -1,41 +1,39 @@
 const path = require('path');
-const express = require('express');
 const http = require('http');
+const express = require('express');
 const socketIO = require('socket.io');
 
-const { generateMessage, generateLocationMessage } = require('./utils/message');
-
-
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
-const app = express();
 const port = process.env.PORT || 3000;
-const server = http.createServer(app);
+var app = express();
+var server = http.createServer(app);
 var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-    console.log('New User connected');
+  console.log('New user connected');
 
-    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
-    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New used joined'));
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-    socket.on('createMessage', (msg, callback) => {
-        console.log('createMessage', msg);
-        io.emit('newMessage', generateMessage(msg.from, msg.text));
-        callback();
-    })
+  socket.on('createMessage', (message, callback) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    callback();
+  });
 
-    socket.on('disconnect', () => {
-        console.log('User was disconected');
-    })
+  socket.on('createLocationMessage', (coords) => {
+    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+  });
 
-    socket.on('createLocationMessage', (coords) => {
-        io.emit('newLocationMessage', generateLocationMessage('Admin', `${coords.latitude}`, `${coords.longitude}`))
-    })
-})
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
+});
 
 server.listen(port, () => {
-    console.log(`Server is up and running on port:${port}`)
-})
+  console.log(`Server is up on ${port}`);
+});
